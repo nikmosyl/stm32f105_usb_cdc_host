609ad990c27ccf1e29ea3b2ec07b6a2f679316bb
nikmosyl <nikmosyl@gmail.com>
2019-09-11 22:22:28 +0300

    start prj

---
+++ cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/DependInfo.cmake
@@ -0,0 +1,87 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/nik/CLionProjects/USBMbeeHost/startup/startup_stm32f105xc.s" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/startup/startup_stm32f105xc.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F105xC"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_hcd.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_hcd.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/main.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/main.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/stm32f1xx_hal_msp.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/stm32f1xx_it.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/stm32f1xx_it.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/syscalls.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/syscalls.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/system_stm32f1xx.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/system_stm32f1xx.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/usb_host.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/usb_host.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/usbh_conf.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/usbh_conf.c.obj"
+  "/Users/nik/CLionProjects/USBMbeeHost/Src/usbh_platform.c" "/Users/nik/CLionProjects/USBMbeeHost/cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/Src/usbh_platform.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F105xC"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/usbMbeeHost.elf.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_hcd.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/main.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/usb_host.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/usbh_conf.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/Src/usbh_platform.c.obj"
+  "CMakeFiles/usbMbeeHost.elf.dir/startup/startup_stm32f105xc.s.obj"
+  "usbMbeeHost.elf.pdb"
+  "usbMbeeHost.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/usbMbeeHost.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
